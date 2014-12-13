8974798ef3dca1b1f22ebbc3b2d7237f0a3ea2a7
Joe Noyes <joe@joenoyes.co.uk>
2014-12-13 15:27:23 +0000

    Update to use bytearray class.

--- classes/hornby/elite.php
+++ classes/hornby/elite.php
@@ -5,7 +5,9 @@
 
 /**
  * Class for controlling the Elite controller
- *
+ * 
+ * Note- Not a singleton as we might have multiple elinks
+ * 
  * @author Caveman
  */
 class hornby_elite 
@@ -17,61 +19,72 @@ class hornby_elite
 	 */
 	protected $__device = NULL;
 	
-	public function __construct($device) 
+	public function __construct($device=NULL, $rate=NULL) 
 	{
-		
+		if($device !== NULL)
+		{
+			$this->set_device();
+		}
+		return $this;
+	}
+	public function set_device()
+	{
+		return $this;
 	}
 	
 	public function get_version()
 	{
 		// Send 212100
-		$data = array(0x21, 0x21, 0x00);
+		//$data = array(0x21, 0x21, 0x00);
 		// First byte is the version
+		$message = bytearray('212100');
+		$response = $device->send($message->as_bin())->read();
+		// If it is 010203, not init return false
+		$response = bytearray::factory()->from_bin($response);
+		$version = reset($response->as_array());
 		return $version;
 	}
-	
+	/**
+	 * Checks whether the device is enabled or not.
+	 * @return boolean
+	 */
 	public function check_init()
 	{
 		// Send 212405
 		// array(0x21,0x24,0x05);
-		$response = $device->send($message)->read();
+		$message = bytearray('212405');
+		$response = $device->send($message->as_bin())->read();
 		// If it is 010203, not init return false
-		
+		$response = bytearray::factory()->from_bin($response);
+		if($response->as_hex() == '010203')
+		{
+			// Not initialised.
+			return false;
+		}
+		return true;
 	}
-	
+	/**
+	 * Initialises the Elite.  
+	 * 
+	 * @throws Kohana_Exception
+	 */
 	public function init()
 	{
 		// Send 3a36344a4b44383942535439
 		//$data = array(0x3a, 0x36, 0x34, 0x4a, 0x4b, 0x44, 0x38, 0x39, 0x42, 0x53, 0x54, 0x39);
-		$message = pack('H*', '3a36344a4b44383942535439');
+		$message = new bytearray('3a36344a4b44383942535439');
 		// Recieve reply such as 35a3680bc56353
-		$response = $device->send($message)->read();
+		$response = $device->send($message->as_bin())->read();
 		// 35 at the start stays the same.
-		$dat = unpack("C*", $response);
 		// Add 0x39 to each of the middle bytes to get 35dca144fe63
-		for($i=2; $i<count($dat); $i++)
-		{
-			$dat[$i] += 0x39;
-		}
-		// Add parity to get 35dca144fe63f2
-		unset($dat[count($dat)]);
-		$par = 0;
-		for($i=2; $i<count($dat); $i++)
-		{
-			$par ^= $dat[$i];
-		}
-		$dat[count($dat)+1] = $par;
-		// Recollapse
-		$message = '';
-		foreach($dat as $d)
-		{
-			$message .= dechex($d);
-		}
+		$response = bytearray::factory()->from_bin($response);
+		$message = $response->add('00393939393900');
+		$message->update_parity();
 		// Send it back
 		$response = $device->send($message)->read();
 		// Should recieve 010405
 		// array(0x01, 0x04, 0x05);
-		if( $response != '010405')
+		if( $response->as_hex() != '010405')
 		{
 			throw new Kohana_Exception('Failed to initialise eLink.');
 		}
