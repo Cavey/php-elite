ad3ac2823ca365c0b8d5ad67456933c263ba547a
Joe Noyes <joe@joenoyes.co.uk>
2014-12-18 18:56:49 +0000

    Update for chainable functions.  Start renaming elite to elink.

--- classes/hornby/elite.php
+++ classes/hornby/elite.php
@@ -4,14 +4,24 @@
  */
 
 /**
- * Class for controlling the Elite controller
+ * Class for controlling the eLink controller
  * 
  * Note- Not a singleton as we might have multiple elinks
  * 
  * @author Caveman
  */
-class hornby_elite 
+class hornby_elink 
 {
+	// No device set
+	public static $NONE = 0;
+	// Device setup
+	public static $SETUP = 1;
+	// Device configured
+	public static $INIT = 3;
+	
+	protected $_status = 0;
+	protected $_version = NULL;
+	
 	/**
 	 * Serial device that respresents the 
 	 * 
@@ -19,6 +29,12 @@ class hornby_elite
 	 */
 	protected $__device = NULL;
 	
+	/**
+	 * 
+	 * @param string $device
+	 * @param integer $rate
+	 * @return \hornby_elink
+	 */
 	public function __construct($device=NULL, $rate=NULL) 
 	{
 		if($device !== NULL)
@@ -27,9 +43,35 @@ class hornby_elite
 		}
 		return $this;
 	}
+	/**
+	 * 
+	 * @param string $device
+	 * @param integer $rate
+	 * @return \hornby_elink
+	 */
 	public function set_device($device=NULL, $rate=NULL)
 	{
-		$this->_device = new PHPSerial($device, $rate);
+		if($this->status == self::$NONE)
+		{
+			$this->_device = new PHPSerial($device, $rate);
+		}
+		return $this;
+	}
+	
+	/**
+	 * 
+	 * @return \hornby_elink
+	 */
+	public function initialise()
+	{
+		if($this->status == self::$SETUP)
+		{
+			$this->get_version();
+			if(!$this->check_init())
+			{
+				$this->init();
+			}
+		}
 		return $this;
 	}
 	
@@ -38,12 +80,15 @@ class hornby_elite
 		// Send 212100
 		//$data = array(0x21, 0x21, 0x00);
 		// First byte is the version
-		$message = new bytearray('212100');
-		$response = $this->_device->send($message->as_bin())->read();
-		// If it is 010203, not init return false
-		$response = bytearray::factory()->from_bin($response);
-		$version = reset($response->as_array());
-		return $version;
+		if($this->_version == NULL)
+		{
+			$message = new bytearray('212100');
+			$response = $this->_device->send($message->as_bin())->read();
+			// If it is 010203, not init return false
+			$response = bytearray::factory()->from_bin($response);
+			$this->_version = reset($response->as_array());
+		}
+		return $this->_version;
 	}
 	/**
 	 * Checks whether the device is enabled or not.
@@ -68,6 +113,7 @@ class hornby_elite
 	 * Initialises the Elite.  
 	 * 
 	 * @throws Kohana_Exception
+	 * @return object horby_elite
 	 */
 	public function init()
 	{
@@ -89,6 +135,7 @@ class hornby_elite
 		{
 			throw new Kohana_Exception('Failed to initialise eLink.');
 		}
+		return $this;
 	}
 }
 
